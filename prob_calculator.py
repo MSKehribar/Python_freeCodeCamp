25e6f7ba61184d2d54175e13e6cc51169aab6cdb
MSKehribar <msk.kehribar@gmail.com>
2022-05-14 16:26:05 +0300

    prob_calculator için düzeltme

--- prob_calculator.py
+++ prob_calculator.py
@@ -1,6 +1,9 @@
+
+# This project written for determine the approjimate probability of drawing certain balls randomly from a hat.
+
 import random
 from copy import deepcopy
-
+import time
 
 class Hat:
     def __init__(self,**kwargs) :
@@ -9,33 +12,41 @@ class Hat:
             for k in range(j):
                 self.icerik.append(i)
 
-    def draw(self, cekilen_top_say):
-        if cekilen_top_say <= len(self.icerik):
+    def draw(self, cek_top_say):
+        sapka_ic_kopyası = deepcopy(hat.icerik)
+        if cek_top_say <= len(sapka_ic_kopyası):
             cekilen_top = []
-            for i in range(cekilen_top_say):
-                rasgele_Deger = random.randrange(len(self.icerik))
-                cekilen_top.append(self.icerik[rasgele_Deger])
-                self.icerik.pop(rasgele_Deger)  
+            for i in range(cek_top_say):
+                rasgele_Deger = random.randrange(len(sapka_ic_kopyası))
+                cekilen_top.append(sapka_ic_kopyası[rasgele_Deger])
+                sapka_ic_kopyası.pop(rasgele_Deger)  
             return cekilen_top
         else:
-            return self.icerik
+            return sapka_ic_kopyası
 
 
 
-def experiment(hat, beklenen_toplar, cekilen_top_say, deney_say):
-    beklenen = []
-    for i, j in beklenen_toplar.items():
-        for k in range(j):
-            beklenen.append(i)
+def experiment(hat, beklenen_toplar, cek_top_say, deney_say):
+
     M = 0
-    content_copy = deepcopy(hat.icerik)
     for i in range(deney_say):
-        if all(item in hat.draw(cekilen_top_say) for item in beklenen):
+        cekilenler=deepcopy(hat.draw(cek_top_say))
+        E = 0
+        for i in beklenen_toplar:
+            if (cekilenler.count(i)>=beklenen_toplar[i]):
+                E = E +1
+        if len(beklenen_toplar)==E:
             M = M + 1
-        hat.icerik = deepcopy(content_copy)
+            
     return M/deney_say
 
 
+
+#Test Part
+baslangic_zamani = time.time()
+
 hat = Hat(black=6, red=4, green=3)
-probability = experiment(hat,{"red":2,"green":1},5,5000)
+probability = experiment(hat,{"red":2,"green":1},5,2000)
 print(probability)
+
+print("--- %s saniye ---" % (time.time() - baslangic_zamani)) 
